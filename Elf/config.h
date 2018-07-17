a03cc1d46028cac7deca59c87c96757767283cd3
muerbingsha <muerbingsha@126.com>
2018-07-17 19:51:34 +0800

    compiled automake project

---
+++ Elf/config.h
@@ -0,0 +1,128 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* have accept4 */
+/* #undef HAVE_ACCEPT4 */
+
+/* have daemon */
+#define HAVE_DAEMON 1
+
+/* Define to 1 if you have the <daemon.h> header file. */
+/* #undef HAVE_DAEMON_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* have epoll */
+/* #undef HAVE_EPOLL */
+
+/* have eventfd */
+/* #undef HAVE_EVENTFD */
+
+/* have execinfo */
+#define HAVE_EXECINFO 1
+
+/* have hiredis */
+/* #undef HAVE_HIREDIS */
+
+/* have inotify */
+/* #undef HAVE_INOTIFY */
+
+/* have inotify_init1 */
+/* #undef HAVE_INOTIFY_INIT1 */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* have kqueue */
+#define HAVE_KQUEUE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* have mkostemp */
+#define HAVE_MKOSTEMP 1
+
+/* have pthread_mutex_timedlock */
+/* #undef HAVE_MUTEX_TIMEDLOCK */
+
+/* have nghttp2 */
+#define HAVE_NGHTTP2 1
+
+/* have openssl */
+#define HAVE_OPENSSL 1
+
+/* have pcre */
+#define HAVE_PCRE 1
+
+/* have poll */
+#define HAVE_POLL 1
+
+/* have pthread_barrier_init */
+/* #undef HAVE_PTHREAD_BARRIER */
+
+/* have pthread_rwlock_init */
+#define HAVE_RWLOCK 1
+
+/* have sendfile */
+#define HAVE_SENDFILE 1
+
+/* have signalfd */
+/* #undef HAVE_SIGNALFD */
+
+/* have pthread_spin_lock */
+/* #undef HAVE_SPINLOCK */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* have timerfd */
+/* #undef HAVE_TIMERFD */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "myserver"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "muerbingsha@126.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "myServer"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "myServer 1.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "myserver"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.0.0"
